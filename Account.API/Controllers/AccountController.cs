71371508d7b6409dfc6eeea3bb9e9efb6bec7e73
vigilija <vigilija.simkeviciute@gmail.com>
2024-01-17 20:43:14 +0100

    Added account endpoint. New  controllerm, models for account and for creation.

---
+++ Account.API/Controllers/AccountController.cs
@@ -0,0 +1,65 @@
+﻿using Account.API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Account.API.Controllers
+{
+    [Route("api/customers/{customerId}/account")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        [HttpGet("{accountId}", Name = "GetAccount")]
+        public ActionResult<AccountDto> GetAccount(string customerId, string accountId)
+        {
+            var customer = CustomersDataStore.Instance.Customers
+                .FirstOrDefault(c => c.Id == customerId);
+            if (customer == null)
+            {
+                return NotFound();
+            }
+
+            // find account for customer
+            var account = customer.Accounts
+                .FirstOrDefault(c => c.Id == accountId);
+
+            if (account == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(account);
+        }
+        [HttpPost]
+        public ActionResult<AccountDto> CreateAccount( string customerId, AccountForCreationDto account)
+        {
+            var customer = CustomersDataStore.Instance.Customers
+                .FirstOrDefault(c => c.Id == customerId);
+
+            if (customer == null)
+            {
+                return NotFound();
+            }
+
+            var newAccount = new AccountDto()
+            {
+                Id = Guid.NewGuid().ToString(),
+                Balance = account.Balance,
+                CreatedDate = DateTime.UtcNow,
+            };
+
+            customer.Accounts.Add(newAccount);
+
+            return CreatedAtRoute("GetAccount",
+                 new
+                 {
+                     customerId = customerId,
+                     accountId = newAccount.Id
+                 },
+                 newAccount);
+        }
+
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+    }
+}
